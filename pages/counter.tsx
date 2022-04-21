37de28f3074be7f368c4a6ab3d34c74f547a91f4
ingong <insong1216@gmail.com>
2022-04-21 17:43:44 +0900

    [FEAT]: 테스트 Counter 페이지 작성

---
+++ pages/counter.tsx
@@ -0,0 +1,24 @@
+import { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import * as counterActions from "../store/modules/counter";
+import { RootState } from "../store/modules";
+
+export default function Test() {
+  const dispatch = useDispatch();
+  const value = useSelector((state: RootState) => state.counter.count);
+  const plus = useCallback(() => {
+    dispatch(counterActions.increment());
+  }, [dispatch]);
+  const minus = useCallback(() => {
+    dispatch(counterActions.decrement());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button onClick={minus}>-</button>
+      <span>{value}</span>
+      <button onClick={plus}>+</button>
+    </div>
+  );
+}
