42727cbe969cecdf40491b1eb93b2c1e3f013aa2
jynam <njy1007@gmail.com>
2022-05-07 16:29:39 +0900

    [chore]: 코드 정리

--- pages/auth/kakao/callback.tsx
+++ pages/auth/kakao/callback.tsx
@@ -1,26 +1,15 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useEffect } from 'react';
 
 const BASE_URL = 'https://asia-northeast3-sannumsan-server.cloudfunctions.net/api';
 
 export default function OAuthRedirectHandler() {
-  const postLogin = async (authenticationCode: string): Promise<any> => {
-    const response = await axios
-      .get(`${BASE_URL}/auth/kakao/callback?code=${authenticationCode}`, {
-        headers: {
-          Accept: `*/*`,
-          'Content-Type': `application/json`,
-        },
-      })
-      .catch((error: AxiosError) => console.error('먀', error));
-    console.error(response as AxiosResponse<any, any>);
-    return response as AxiosResponse<any, any>;
-  };
-
   useEffect(() => {
     const authenticationCode = new URL(window.location.href).searchParams.get('code') ?? '';
     (async () => {
-      await postLogin(authenticationCode);
+      await axios
+        .get(`${BASE_URL}/auth/kakao/callback?code=${authenticationCode}`)
+        .catch((error: AxiosError) => console.log('먀', error));
     })();
   }, []);
   return <></>;
