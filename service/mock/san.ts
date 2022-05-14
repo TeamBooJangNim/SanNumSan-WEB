b5e8aa9c4ace76f79f50711cdfdaf3484fe16a85
jynam <njy1007@gmail.com>
2022-05-14 09:51:58 +0900

    [feat]: 인증 정보 get mock api 만들기

--- service/mock/san.ts
+++ service/mock/san.ts
@@ -17,5 +17,13 @@ export function sanMock(): SanService {
     };
   }
 
-  return { getSanDetail };
+  async function getProveDetail(proveID: number) {
+    return {
+      id: proveID,
+      image: 'https://user-images.githubusercontent.com/73823388/168329553-4e2acea9-26b7-4d23-8af4-27fa8f1c639a.png',
+      comment: '북한산 등반 완료 우하하',
+    };
+  }
+
+  return { getSanDetail, getProveDetail };
 }
