9dd1d5b7b5e16064442c3f62505a88431f0830da
seojin kim <001106ksj@gmail.com>
2022-05-15 01:44:54 +0900

    [fix] Pretendard의 진실 발견 및 Pretendard 미적용 이슈 해결
    * reset.scss의 비밀 발견
    * auto format 때문에 어쩔 수 없으므로 base 폴더 안에 집어넣음

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import type { AppProps } from 'next/app';
 import '../component/BottomSheetModal/BottomSheetModal.scss';
 import '../component/FloatingModal/FloatingModal.scss';
 import { wrapper } from '../store';
-import '../styles/globals.scss';
+import '../styles/base/globals.scss';
+import '../styles/base/reset.scss';
 import '../styles/prove-detail.scss';
-import '../styles/reset.scss';
 import '../styles/san-detail.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
